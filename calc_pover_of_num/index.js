a5a75afa139f8238d549c605920907861d96f769
Annetkut <annakutuzova.prodesign@gmail.com>
2023-01-29 21:02:41 +0300

    add webpage files

---
+++ calc_pover_of_num/index.js
@@ -0,0 +1,10 @@
+let Input = document.getElementById("a");
+let squareBtn = document.getElementById("square");
+let result = document.getElementById("result");
+
+
+function calcSquare(){
+    let a = +(Input.value);
+    result.innerHTML = "результат: " + (a**2);
+}
+squareBtn.onclick = calcSquare;
