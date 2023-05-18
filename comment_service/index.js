55e47fb3d190a7ec1712ee13fe7426ca08993c9e
Annetkut <annakutuzova.prodesign@gmail.com>
2023-05-19 01:08:43 +0300

    added css styles

--- comment_service/index.js
+++ comment_service/index.js
@@ -6,59 +6,55 @@ const avatarInput = document.getElementById("avatar");
 const commentInput = document.getElementById("comment");
 const my_form = document.getElementById("my_form");
 
-const resultImage = document.querySelector(".chat_img");
-const resultUser = document.querySelector(".chat_username");
-const resultMessage = document.querySelector(".chat_message");
+const resultImage = document.querySelector('.chat_img');
+const resultUser = document.querySelector('.chat_username');
+const resultMessage = document.querySelector('.chat_message');
+
 
 function handleLowerUpperCase(text) {
-  // переписывает в нижний регистр и разделяет строку на слова, создавая массив
-  // const words = text.toLowerCase().split(' ');
-  text = text.toLowerCase();
-  text = text.replace(/\s+/g, " ");
-  const words = text.split(" "); // words = ["фамилия", "имя", "очество"]
+    // переписывает в нижний регистр и разделяет строку на слова, создавая массив
+    // const words = text.toLowerCase().split(' ');
+    text = text.toLowerCase();
+    text = text.replace(/\s+/g, " ");
+    const words = text.split(' '); // words = ["фамилия", "имя", "очество"]
 
-  if (words.length == 1) {
-    return words[0].replace(words[0][0], words[0][0].toUpperCase());
-  } else if (words.length == 2) {
-    const new_family_name = words[0].replace(
-      words[0][0],
-      words[0][0].toUpperCase()
-    );
-    const new_name = words[1].replace(words[1][0], words[1][0].toUpperCase());
-    return new_family_name + " " + new_name;
-  } else if (words.length == 3) {
-    const new_family_name = words[0].replace(
-      words[0][0],
-      words[0][0].toUpperCase()
-    );
-    const new_name = words[1].replace(words[1][0], words[1][0].toUpperCase());
-    const otschestvo = words[2].replace(words[2][0], words[2][0].toUpperCase());
-    return new_family_name + " " + new_name + " " + otschestvo;
-  } else {
-    return "Error!";
-  }
+    if (words.length == 1) {
+        return words[0].replace(words[0][0], words[0][0].toUpperCase());
+    } else if (words.length == 2) {
+        const new_family_name = words[0].replace(words[0][0], words[0][0].toUpperCase());
+        const new_name = words[1].replace(words[1][0], words[1][0].toUpperCase());
+        return new_family_name + " " + new_name
+    } else if (words.length == 3) {
+        const new_family_name = words[0].replace(words[0][0], words[0][0].toUpperCase());
+        const new_name = words[1].replace(words[1][0], words[1][0].toUpperCase());
+        const otschestvo = words[2].replace(words[2][0], words[2][0].toUpperCase());
+        return new_family_name + " " + new_name + " " + otschestvo;
+    } else {
+        return "Error!";
+    }
 }
 
 const checkSpam = (text) => {
-  return text.replace(/viagra|XXX/gi, "***");
+    return text.replace(/viagra|XXX/ig, "***");
 };
 
+
 function viewAll(event) {
-  event.preventDefault(); // prevent form submission предотвращать отправление формы‚
-  // поменять регистр фио пользователя
-  let convertedUserName = handleLowerUpperCase(nameInput.value);
-  if (convertedUserName === "Error!") {
-    alert("Введите фамилию, имя и очество!");
-  } else {
-    // вставить картинку по ссылке пользователя
-    resultImage.style.backgroundImage = `url(${avatarInput.value})`;
-    // вставить поправленное фио пользователя в нужный htmpl тэг
-    resultUser.innerHTML = convertedUserName;
-    // зацензурить текст сообщения пользователя и вставить в нужный htmpl тэг
-    resultMessage.innerHTML = checkSpam(commentInput.value);
-  }
-  my_form.reset();
+    event.preventDefault(); // prevent form submission предотвращать отправление формы‚
+    // поменять регистр фио пользователя
+    let convertedUserName = handleLowerUpperCase(nameInput.value);
+    if (convertedUserName === "Error!") {
+        alert("Введите фамилию, имя и очество!");
+    } else {
+        // вставить картинку по ссылке пользователя
+        resultImage.style.backgroundImage = `url(${avatarInput.value})`;
+        // вставить поправленное фио пользователя в нужный htmpl тэг
+        resultUser.innerHTML = convertedUserName;
+        // зацензурить текст сообщения пользователя и вставить в нужный htmpl тэг
+        resultMessage.innerHTML = checkSpam(commentInput.value);
+    }
+    my_form.reset();
 }
 
 // document.getElementById("btn").onclick = viewAll;
-document.getElementById("btn").addEventListener("click", viewAll);
+document.getElementById('btn').addEventListener('click', viewAll);
